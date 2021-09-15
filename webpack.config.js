7b348f85b3ca9e35160d1090eb071aed91a04fc6
kimiashz <shzkimia@gmail.com>
2021-09-15 19:09:07 +0430

    add build script

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
     output : {
         path : path.resolve(__dirname , 'dist'),
         filename: 'index_bundle.js',
-        publicPath: '/',
     },
     module : {
         rules : [
