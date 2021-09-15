d68d46edece7924a979452406b8f237db63bd97a
kimiashz <shzkimia@gmail.com>
2021-09-15 21:02:33 +0430

    add Alert for Lazy loading

--- app/components/Layout/components/Content/index.jsx
+++ app/components/Layout/components/Content/index.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import classNames from 'classnames';
 import { Switch, Route, Redirect } from "react-router-dom";
 
+import Alert from '/app/components/Alert/index.jsx';
 import routes from '/app/config/routes.js';
 import * as paths from '/app/config/paths.js';
 
@@ -13,10 +14,11 @@ function Content({
     className: classNameProp,
 }) {
     const className = classNames(BLOCK, classNameProp);
+    const Loading = <Alert info message="Lazy Component is loading..." />;
 
     return (
         <article className={className}>
-            <Suspense fallback={<div>is Loading...</div>}>
+            <Suspense fallback={Loading}>
                 <Switch>
                     <Redirect exact from="/" to={paths.HOME} replace />
                     {routes.map(route => (
